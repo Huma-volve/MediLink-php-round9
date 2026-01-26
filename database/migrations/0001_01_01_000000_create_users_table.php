3814edb964cf0e2f7688002a23d2d409457d74d7
menna567647 <menna911reda@gmail.com>
2026-01-26 20:39:35 +0200

    migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('full_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'doctor', 'patient'])->default('patient');
+            $table->string('profile_picture')->nullable();
+            $table->enum('gender', ['male', 'female'])->nullable();
+            $table->boolean('is_active')->default('true');
+            $table->string('phone')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
