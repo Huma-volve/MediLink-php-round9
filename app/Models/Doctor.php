9bdea4e04b2b5305ad6f2ea4008677a944b3860f
Ahmad-Asban <ahmdmrsban99@email.com>
2026-01-27 21:45:32 +0300

    created an api endpoint to fetch top rated doctors and make the doctor resource

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -7,6 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Doctor extends Model
 {
 
+    protected $fillable = [
+        'user_id',
+        'license_number',
+        'experience_years',
+        'certification',
+        'bio',
+        'education',
+        'consultation_fee_online',
+        'consultation_fee_inperson',
+        'spelization_id',
+        'location',
+        'is_verified',
+    ];
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
@@ -18,13 +19,14 @@ Route::middleware('auth:sanctum')->group(function () {
         ]);
     });
 });
+
 use App\Http\Controllers\Api\DoctorController;
 use App\Http\Controllers\api\v1\GeneralController;
 use App\Http\Controllers\api\v1\PatientController;
 
 Route::group(['prefix' => 'v1'], function () {
     Route::get('spelizations', [GeneralController::class, 'spelizations']);
-  Route::get('/doctors/search', [DoctorController::class, 'search']);
+    Route::get('/doctors/search', [DoctorController::class, 'search']);
 });
 
 
@@ -32,3 +34,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
+
+
+Route::get('/top-rated-doctors', [DoctorController::class, 'topRatedDoctors']);
