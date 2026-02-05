8d69815285e5808e0f3701977c85eef6bcdc88e1
Ahmad-Asban <ahmdmrsban99@email.com>
2026-02-05 12:11:14 +0300

    re push

--- app/Filament/Resources/AppSettings/Pages/EditAppSetting.php
+++ app/Filament/Resources/AppSettings/Pages/EditAppSetting.php
@@ -12,8 +12,6 @@ class EditAppSetting extends EditRecord
 
     protected function getHeaderActions(): array
     {
-        return [
-            // لا Delete
-        ];
+        return [];
     }
 }
